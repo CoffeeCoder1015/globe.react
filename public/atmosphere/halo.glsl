3d6b392a7b4a1d5450e44bbf0e06d4d01ca65a99
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-08-07 22:32:11 +1200

    halo locked in

--- public/atmosphere/halo.glsl
+++ public/atmosphere/halo.glsl
@@ -8,5 +8,24 @@ uniform float radiusEarth;
 uniform float radiusAtmosphere;
 
 void main(){
-    gl_FragColor = vec4(1.-distance(apparent_pos.xy,vec2(0.)));
+    vec3 con = normalize( cameraPosition );
+    float dist = distance(apparent_pos.xy,vec2(0.));
+    // reverse halo effect
+    float bloomintensity = dot(vNormal,con);
+    float sun_on_lignt = dot(normalize(lightPosition),con);
+    float intensity = bloomintensity*pow(0.8,dist*radiusAtmosphere);
+    
+    // general lighting
+    vec3 lightDirection = lightPosition-vNormal*radiusAtmosphere;
+    float lightingIntensity = dot(normalize(lightDirection), vNormal);
+    
+    // camera
+    float t = dot(con,radiusAtmosphere/2.*vNormal); // ig its not really the radius?!
+    vec4 blueHue = vec4(0.5,0.6,1.,1.);
+    blueHue.xyz *= lightingIntensity+gl_FragColor.xyz;
+    blueHue.xyz = clamp(blueHue.xyz,0.2,7.);
+    blueHue.w = clamp(lightingIntensity,0.3,0.7);
+    gl_FragColor = blueHue;
+    // gl_FragColor = blueHue*gl_FragColor+lightingIntensity/4.;
+    gl_FragColor.xyz*=intensity*t/2.*(1.-sun_on_lignt*0.5);
 }
